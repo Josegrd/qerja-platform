09d0198a877ea15e7cb3e74c2d8fe193d959b8ea
gardio <josegarda8@gmail.com>
2025-01-08 01:21:43 +0700

    add delete feature

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
@@ -13,6 +16,8 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        $categories = Category::all();
+        return view("admin.categories.index", compact("categories"));
     }
 
     /**
@@ -21,14 +26,29 @@ class CategoryController extends Controller
     public function create()
     {
         //
+        return view("admin.categories.create");
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
         //
+        DB::transaction(function () use ($request) {
+           $validated = $request->validated();
+
+           if ($request->hasFile("icon")) {
+               $iconPath = $request->file("icon")->store("icons", "public");
+               $validated["icon"] = $iconPath;
+           }
+
+           $validated["slug"] = Str::slug($validated["name"]);
+
+           $newCategory = Category::create($validated);
+        });
+
+        return redirect()->route("admin.categories.index");
     }
 
     /**
@@ -61,5 +81,17 @@ class CategoryController extends Controller
     public function destroy(Category $category)
     {
         //
+        // dd($category);
+
+        DB::beginTransaction();
+
+        try{
+            $category->delete();
+            DB::commit();
+            return redirect()->route("admin.categories.index");
+        }catch(\Exception $e){
+            DB::rollBack();
+            return redirect()->route("admin.categories.index");
+        }
     }
 }
